777254ff01b7dda3825ca6c163f17ba3982e1cef
Your Name <you@example.com>
2022-05-06 17:08:25 +0200

    1er Commit - Comienzo con servidor escuchando

---
+++ casas.js
@@ -0,0 +1,21 @@
+//jshint esversion:6
+//Inicializo los módulos que instalé con npm
+
+// 1.Express
+
+const express=require("express");
+const app=express();
+app.use(express.static("public"));
+
+// 2.body-parser
+const bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
+
+// Zona de enrutamiento
+app.get("/", function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
+// 3.Pongo a escuchar el server
+app.listen(4001,function(){
+  console.log("Estoy escuchando en el puerto 4001");
+});
